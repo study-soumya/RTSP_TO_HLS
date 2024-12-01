8950ce5843b644f88685c09e48703f7d14a8798d
study-soumya <sonusoum.study@gmail.com>
2024-12-01 20:01:07 +0530

    second commit

--- stream/serializers.py
+++ stream/serializers.py
@@ -3,6 +3,11 @@ from .models import *
 
 
 class StreamSerializer(serializers.ModelSerializer):
+    hls_url = serializers.SerializerMethodField()
     class Meta:
         model = Stream
-        fields = '__all__'
+        fields = ['id', 'ip_address', 'username', 'password', 'place', 'hls_url']
+
+    def get_hls_url(self, obj):
+        # Construct the HLS URL
+        return f"/streams/{obj.ip_address}/stream.m3u8"
